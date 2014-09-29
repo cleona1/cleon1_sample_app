9045511ee0bd77c353181966974d164668c80036
cleona1 <cleon1@umbc.edu>
2014-09-28 22:25:44 -0400

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,35 +28,34 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
+  
 
     
 
-describe "with valid information" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
+       fill_in "Password", with: user.password
         click_button "Sign in"
       end
-  
+    end
 
-      it { should have_title(user.name) }
+    it { should have_selector('title', text: user.name) }
 
-     # it { should have_link('Profile',     href: user_path(user)) }
-     # it { should have_link('Sign out',    href: signout_path) }
-      #it { should_not have_link('Sign in', href: signin_path) }
-  
-  
+    it { should have_link('Users', href: users_path) }
+    it { should have_link('Profile', href: user_path(user)) }
+    it { should have_link('Settings', href: edit_user_path(user)) }
+    it { should have_link('Sign out', href: signout_path) }
+    it { should_not have_link('Sign in', href: signin_path) }
 
-    
-#describe "followed by signout" do
-      #  before { click_link "Sign out" }
-      #  it { should have_link('Sign in') }
-     # end
- 
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+    describe "followed by signout" do
+      before { click_link "Sign out" }
+      it { should have_link('Sign in') }
+      end
     end
   end
 end
+  
+  
+    
+  

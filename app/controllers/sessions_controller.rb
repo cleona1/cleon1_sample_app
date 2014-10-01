56c01b5422e298022442d183b0198da574ec8a34
cleona1 <cleon1@umbc.edu>
2014-10-01 19:42:36 -0400

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,3 @@
-
 =begin
 
 class SessionsController < ApplicationController
@@ -22,15 +21,16 @@ end
 =end
 
 
+
 class SessionsController < ApplicationController
 def new
 end
 def create
 user = User.find_by_email(params[:session][:email].downcase)
 if user && user.authenticate(params[:session][:password])
-sign_in user
-redirect_back_or user
-else
+#sign_in user
+#redirect_back_or user
+#else
 flash.now[:error] = 'Invalid email/password combination'
 render 'new'
 end

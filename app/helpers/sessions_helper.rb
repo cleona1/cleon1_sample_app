8e309fb0d75687ea5fb6e1b36589edf1fa3ad6fb
cleona1 <cleon1@umbc.edu>
2014-10-02 18:59:37 -0400

    Finish sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,7 @@
-=begin
+
+
 module SessionsHelper
+
 
   def sign_in(user)
     remember_token = User.new_remember_token
@@ -8,48 +10,23 @@ module SessionsHelper
     self.current_user = user
   end
 
-  def signed_in?
-    !current_user.nil?
-  end
-
-
-   def current_user=(user)
-    @current_user = user
-  end
-
-  def current_user
-    remember_token = User.digest(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
-  end
-end
-=end
-
-
-
-
-
-
-
-module SessionsHelper
-
-
-def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.digest(remember_token))
-    self.current_user = user
-  end
-
 
 def signed_in?
-!current_user.nil?
-end
+    !current_user.nil?
+  end
 def current_user=(user)
 @current_user = user
 end
+=begin
 def current_user
 @current_user ||= User.find_by_remember_token(cookies[:remember_token])
 end
+=end
+def current_user
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
 def current_user?(user)
 user == current_user
 end
@@ -71,12 +48,12 @@ def store_location
 session[:return_to] = request.url
 end
 
-def sign_out
+  def sign_out
     current_user.update_attribute(:remember_token,
                                   User.digest(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
- 
   end
 end
+
 

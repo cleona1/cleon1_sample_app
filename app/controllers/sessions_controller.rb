3bb78c4f2a74e87b6b26b600060b18218be2d2cd
cleona1 <cleon1@umbc.edu>
2014-10-04 13:52:06 -0400

    Fix signout_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,6 @@ class SessionsController < ApplicationController
 def new
 end
 
-=begin
-def create
-user = User.find_by_email(params[:session][:email].downcase)
-if user && user.authenticate(params[:session][:password])
-#sign_in user
-redirect_back_or user
-else
-flash.now[:error] = 'Invalid email/password combination'
-render 'new'
-end
-=end
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  #before_action :signed_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -22,11 +23,30 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
+
+
+   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
 end
 

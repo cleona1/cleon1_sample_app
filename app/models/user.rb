da3986291c9ad59ec405e1570357489effee4115
cleona1 <cleon1@umbc.edu>
2014-10-11 20:44:32 -0400

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,8 +34,9 @@ def User.new_remember_token
   end
 
 def feed
+    Micropost.from_users_followed_by(self)
     # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+   # Micropost.where("user_id = ?", id)
   end
 
  def following?(other_user)
